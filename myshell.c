044eee4fa039f499c37f545aaeec63544e3b993f
Amit Shalvi Kali <a.shalvikali@gmail.com>
2025-12-08 14:33:49 +0200

    error handling + background commands

--- myshell.c
+++ myshell.c
@@ -3,6 +3,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define GENERAL_SUCCESS (0)
 #define GENERAL_FAILURE (-1)
@@ -47,29 +50,37 @@ bool is_output_redirection_command(int count, char** arglist)
     return false;
 }
 
-int run_foreground_command(int count, char** arglist)
+int run_command(int count, char** arglist, bool should_wait)
 {
+    int return_code = GENERAL_FAILURE;
     int child_exit_code = -1;
     pid_t pid = fork();
     if (-1 == pid) {
-        // TODO: error handling
-        return GENERAL_FAILURE;
+        perror("fork failed");
+        goto cleanup;
     } else if (0 == pid) {
         // child process
         if (-1 == execvp(arglist[0], arglist)) {
-            // should not return here unless error
-            // TODO: error handling
-            return GENERAL_FAILURE;
+            // should not return here unless error.
+            // this does not cause the shell (parent process) to exit, only the child process.
+            perror("execvp failed");
+            exit(1);
         }
     } else {
         // parent process
-        if (-1 == waitpid(pid, &child_exit_code, 0)) {
-            // TODO: error handling
-            return GENERAL_FAILURE;
+        if (should_wait) {
+            // ECHILD and EINTR are not considered an actual error that requires exiting the shell.
+            if ((-1 == waitpid(pid, &child_exit_code, 0)) && (errno != ECHILD) && (errno != EINTR)) {
+                perror("waitpid failed");
+                goto cleanup;
+            }
+            // do we even care about the child's exit code?
         }
-        // do we even care about the child's exit code?
     }
-    return GENERAL_SUCCESS;
+
+    return_code = GENERAL_SUCCESS;
+cleanup:
+    return return_code;
 }
 
 int prepare(void)
@@ -93,13 +104,16 @@ int process_arglist(int count, char** arglist)
     if (is_piping_command(count, arglist)) {
         // handle piping commands
     } else if (is_background_command(count, arglist)) {
-        // handle background commands
+        arglist[count - 1] = NULL; // Do not pass the & argument to execvp().
+        if (run_command(count, arglist, false) != GENERAL_SUCCESS) {
+            goto cleanup;
+        }
     } else if (is_input_redirection_command(count, arglist)) {
         // handle input redirection commands
     } else if (is_output_redirection_command(count, arglist)) {
         // handle output redirection commands
     } else {
-        if (run_foreground_command(count, arglist) != GENERAL_SUCCESS) {
+        if (run_command(count, arglist, true) != GENERAL_SUCCESS) {
             goto cleanup;
         }
     }
